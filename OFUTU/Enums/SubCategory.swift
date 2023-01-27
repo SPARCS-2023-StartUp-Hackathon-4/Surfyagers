9893c1f0abcd724e557e22a0704bf554724e64d1
myungsun <audtjs9987@naver.com>
2023-01-28 02:12:20 +0900

    feat: make SubCategory enum

---
+++ OFUTU/Enums/SubCategory.swift
@@ -0,0 +1,18 @@
+//
+//  SubCategory.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import Foundation
+
+enum SubCategory: String {
+    case home = "홈"
+    case zeroWaste = "제로웨이스트"
+    case fairTrade = "공정무역"
+    case ecoFriendly = "친환경"
+    case vegun = "비건"
+    case donation = "기부"
+    case etc = "ETC"
+}
