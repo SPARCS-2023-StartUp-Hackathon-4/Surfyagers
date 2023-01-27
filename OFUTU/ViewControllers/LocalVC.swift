eba4037b51bb25ce866596a7d21aad3b2002072d
myungsun <audtjs9987@naver.com>
2023-01-28 01:31:40 +0900

    chore: add LocalVC class

---
+++ OFUTU/ViewControllers/LocalVC.swift
@@ -0,0 +1,16 @@
+//
+//  LocalVC.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import UIKit
+
+class LocalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
