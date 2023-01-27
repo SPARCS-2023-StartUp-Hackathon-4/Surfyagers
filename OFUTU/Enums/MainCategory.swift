11534f9e7e5a0631b4197bdae896c7699a2228c9
myungsun <audtjs9987@naver.com>
2023-01-28 02:10:16 +0900

    feat: make MainCategory enum

---
+++ OFUTU/Enums/MainCategory.swift
@@ -0,0 +1,13 @@
+//
+//  MainCategory.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import Foundation
+
+enum MainCategory: String {
+    case main = "메인"
+    case community = "커뮤니티"
+}
