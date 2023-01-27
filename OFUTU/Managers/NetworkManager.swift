b148dac93bccc42d4d0304439db40b851ab42ff0
myungsun <audtjs9987@naver.com>
2023-01-28 02:08:11 +0900

    feat: develop NetworkManager class

--- OFUTU/Managers/NetworkManager.swift
+++ OFUTU/Managers/NetworkManager.swift
@@ -5,4 +5,54 @@
 //  Created by myungsun on 2023/01/28.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func creatUser(userObj: User) {
+        let REQUEST_URL = ""
+        let PARAMS: [String: Any] = [:]
+        let HEADERS: HTTPHeaders = [:]
+        AF.request(REQUEST_URL,
+                   method: .put,
+                   parameters: PARAMS,
+                   encoding: URLEncoding.default,
+                   headers: HEADERS)
+        .validate(statusCode: 200..<300)
+        .response { response in
+            switch response.result {
+            case .success(let data):
+                let json = JSON(data!)
+            case .failure(_):
+                print(JSON(response.data))
+                print(response.error)
+            }
+        }
+    }
+    
+    func fetchUser(completionHandler: @escaping (_ userObj: User)-> ()) {
+        let REQUEST_URL = ""
+        let PARAMS: [String: Any] = [:]
+        let HEADERS: HTTPHeaders = [:]
+        AF.request(REQUEST_URL,
+                   method: .get,
+                   parameters: PARAMS,
+                   encoding: URLEncoding.default,
+                   headers: HEADERS)
+        .validate(statusCode: 200..<300)
+        .response { response in
+            switch response.result {
+            case .success(let data):
+                let json = JSON(data!)
+            case .failure(_):
+                print(JSON(response.data))
+                print(response.error)
+            }
+        }
+    }
+}
