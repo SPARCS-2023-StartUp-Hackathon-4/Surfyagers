ec80d4fe2409b075d8182f7b109b4593aca2c9fc
myungsun <audtjs9987@naver.com>
2023-01-28 02:17:55 +0900

    chore: add MainTabBarController class

---
+++ OFUTU/ViewControllers/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
