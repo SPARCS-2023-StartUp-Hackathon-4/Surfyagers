995691880efa435feaa10c36a7b9a1167de39d32
myungsun <audtjs9987@naver.com>
2023-01-28 01:27:52 +0900

    chore: add CategoryVC class

---
+++ OFUTU/ViewControllers/CategoryVC.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryVC.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
