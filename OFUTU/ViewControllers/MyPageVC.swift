9b19b38f5f16da55f59a65a20a29b2f73ef4c032
myungsun <audtjs9987@naver.com>
2023-01-28 01:35:17 +0900

    chore: add MyPageVC class

---
+++ OFUTU/ViewControllers/MyPageVC.swift
@@ -0,0 +1,15 @@
+//
+//  MyPageVC.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import UIKit
+
+class MyPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
