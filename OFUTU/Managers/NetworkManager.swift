9ea743253726ebf872506ab087016a7b17574d88
myungsun <audtjs9987@naver.com>
2023-01-28 02:07:40 +0900

    feat: make User DTO Model

---
+++ OFUTU/Managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import Foundation
---
+++ OFUTU/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  OFUTU
+//
+//  Created by myungsun on 2023/01/28.
+//
+
+import UIKit
+
+struct User {
+    var name: String
+    var tagList: [String]
+    var goal: String
+    var beginDay: Int
+}
