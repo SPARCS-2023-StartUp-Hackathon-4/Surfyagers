716080a3ada5e7a9c65b38723ef193fed012b7af
myungsun <audtjs9987@naver.com>
2023-01-29 01:26:33 +0900

    feat: compose DetailLocal Cell

--- OFUTU/Cells/DetailLocalCell.swift
+++ OFUTU/Cells/DetailLocalCell.swift
@@ -6,18 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class DetailLocalCell: UITableViewCell {
-
+    // UIButton
+    @IBOutlet weak var seeButton: UIButton!
+    
+    // Constants
+    let BUTTON_BORDER_WIDTH: CGFloat = 1
+    let BUTTON_CORNER_RADIUS: CGFloat = 3
+    let BUTTON_FONT_SIZE: CGFloat = 12
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configureButton()
     }
     
+    private func configureButton() {
+        seeButton.layer.borderColor = ColorManager.shared.getGainsboro().cgColor
+        seeButton.layer.borderWidth = BUTTON_BORDER_WIDTH
+        seeButton.layer.cornerRadius = BUTTON_CORNER_RADIUS
+        seeButton.titleLabel?.font = FontManager.shared.getPretendardMedium(fontSize: BUTTON_FONT_SIZE)
+        seeButton.snp.makeConstraints { make in
+            make.width.equalTo(65)
+        }
+    }
+
 }
