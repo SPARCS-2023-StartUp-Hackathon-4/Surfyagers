b2cc22d9c367bd83a88f79ad893a8f055ae0594b
myungsun <audtjs9987@naver.com>
2023-01-28 16:14:38 +0900

    feat: develop UserDefaultsManager class

--- OFUTU/Managers/UserDefaultsManager.swift
+++ OFUTU/Managers/UserDefaultsManager.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    
+    private init() {}
+    
+    private let USER_NAME = "userName"
+    private let TAG_List = "tagList"
+    private let USER_GOAL = "userGoal"
+    
+    func getUserName() -> String {
+        return UserDefaults.standard.string(forKey: USER_NAME)!
+    }
+    
+    func setUserName(name: String) {
+        UserDefaults.standard.set(name, forKey: USER_NAME)
+    }
+    
+    func setUserTagList(tagList: [String]) {
+        UserDefaults.standard.set(tagList, forKey: TAG_List)
+    }
+    
+    func getUserTagList() -> [String] {
+        return UserDefaults.standard.object(forKey: TAG_List) as! [String]
+    }
+    
+    func getUserGoal() -> String {
+        return UserDefaults.standard.string(forKey: USER_GOAL)!
+    }
+    
+    func setUserGoal(goal: String) {
+        UserDefaults.standard.set(goal, forKey: USER_GOAL)
+    }
+}
